e705d52a81a2699bec6522cd1ba710bf30a2b710
Claudia Queipo <claudia.queipog@gmail.com>
2024-11-01 17:31:28 -0400

    chore(docker-compose): docker compose and dockerfile implemented for the api deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common'
-import { ApiOperation } from '@nestjs/swagger'
 import { AppService } from './app.service'
 
 @Controller('health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @ApiOperation({ summary: 'Check server health' })
   @Get()
   getHealth(): string {
     return this.appService.health()
